ccaab823a892e11dd6022baccda4c0c44cb6b37a
Michael Knopacki <michaelknopacki@hotmail.com>
2024-10-10 19:59:57 -0300

    20241010

--- Desafios/4.11 - Lista de exercícios.js
+++ Desafios/4.11 - Lista de exercícios.js
@@ -78,4 +78,73 @@ for (let i =0; i < botoes.length; i++){
 
 ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+4) Utilizando método while para aplicar contador : 
+No aplicativo de teclado MIDI virtual, você se deparou com um novo desafio: Utilize o método while para aplicar a funcionalidade de incrementar um contador em mais um, 
+e o valor atualizado do contador deve ser exibido no console do navegador. Agora, isso deve acontecer cada vez que algum botão da lista de botões for clicado.
+
+-> Index.html
+
+<input onclick="clicado(event)" type="button" class="meuBotao" value="1" />
+<input onclick="clicado(event)" type="button" class="meuBotao" value="2" />
+<input onclick="clicado(event)" type="button" class="meuBotao" value="3" />
+<input onclick="clicado(event)" type="button" class="meuBotao" value="4" />
+<input onclick="clicado(event)" type="button" class="meuBotao" value="5" />
+
+-> Script.js
+
+const botoes = document.querySelectorAll('.meuBotao');
+//console.log(botoes);
+//console.log('Quantidade de  botões ' + botoes.length);
+
+//Criada uma variável onde é guardado o value do quarto elemento
+const quartoElemento = botoes[4];
+console.log('Quarto elemento : ' + quartoElemento.value);
+
+//Imprime no console um valor quando um botão é clicado
+function clicado(event) {
+  console.log(event.target.value);
+  //Qualquer botão QUALQUER botão for clicado a condição ENQUANTO é ativada
+  i = 0;
+  // condição de ENQUANTO i for menor que a quantidade de botões ele executa o que está na função.
+  while (i < botoes.length) {
+    console.log('Botão ' + botoes[i].value);
+    i = i + 1;
+  }
+}
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+5) Utilizando método for para aplicar contador : 
+Chegaram nova demandas no aplicativo de teclado MIDI virtual: Refatore o código realizado anteriormente nesta lista de exercícios, 
+utilizando o método for do JavaScript para realizar a mesma tarefa, incrementar o contador em mais um, e exibir o resultado no console do navegador, cada vez que algum botão da lista de botões for clicado.
+
+-> Index.html
+
+<input onclick="clicado(event)" type="button" class="meuBotao" value="1" />
+<input onclick="clicado(event)" type="button" class="meuBotao" value="2" />
+<input onclick="clicado(event)" type="button" class="meuBotao" value="3" />
+<input onclick="clicado(event)" type="button" class="meuBotao" value="4" />
+<input onclick="clicado(event)" type="button" class="meuBotao" value="5" />
+
+-> Script.js
+
+const botoes = document.querySelectorAll('.meuBotao');
+//console.log(botoes);
+//console.log('Quantidade de  botões ' + botoes.length);
+
+//Criada uma variável onde é guardado o value do quarto elemento
+const quartoElemento = botoes[4];
+console.log('Quarto elemento : ' + quartoElemento.value);
+
+//Imprime no console um valor quando um botão é clicado
+function clicado(event) {
+  console.log(event.target.value);
+  //Qualquer botão QUALQUER botão for clicado a condição ENQUANTO é ativada
+
+  // condição de FOR i for menor que a quantidade de botões ele executa o que está na função.
+  for (i = 0; i < botoes.length; i++) {
+    console.log('Botão ' + botoes[i].value);
+  }
+}
+
 */
